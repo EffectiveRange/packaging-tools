4fd285f0226e60390d8569294761d0439062ff07
Attila Gombos <attila.gombos@effective-range.com>
2024-08-04 16:49:04 +0200

    Propagate return code of subprocess calls

--- python/tests/dhVirtualenvTest.py
+++ python/tests/dhVirtualenvTest.py
@@ -4,7 +4,7 @@ import unittest
 from unittest import TestCase
 
 from utils import TEST_PROJECT_ROOT, TEST_RESOURCE_ROOT, TEST_FILE_SYSTEM_ROOT, \
-    delete_directory, check_file_is_in_deb, SCRIPTS_DIR, run_command, check_files_matches_in_deb
+    delete_directory, check_file_is_in_deb, RESOURCE_ROOT, run_command, check_files_matches_in_deb, check_files_exist
 
 
 class DhVirtualenvTest(TestCase):
@@ -16,7 +16,7 @@ class DhVirtualenvTest(TestCase):
 
     def test_dh_virtualenv_when_no_output_dir_specified(self):
         # Given
-        command = [f'{SCRIPTS_DIR}/dh-virtualenv', TEST_PROJECT_ROOT]
+        command = [f'{RESOURCE_ROOT}/dh-virtualenv', TEST_PROJECT_ROOT]
 
         # When
         result = run_command(command)
@@ -24,11 +24,12 @@ class DhVirtualenvTest(TestCase):
         # Then
         self.assertEqual(0, result.returncode)
         self.assertEqual(f'{TEST_PROJECT_ROOT}/dist/test-project_1.0.0-1_all.deb\n', result.stdout)
+        self.assertTrue(check_files_exist(result.stdout))
 
     def test_dh_virtualenv_when_relative_output_dir_specified(self):
         # Given
         output_dir = 'tests/test_root/etc/dist' if os.path.exists('tests') else 'test_root/etc/dist'
-        command = [f'{SCRIPTS_DIR}/dh-virtualenv', TEST_PROJECT_ROOT, '-o', output_dir]
+        command = [f'{RESOURCE_ROOT}/dh-virtualenv', TEST_PROJECT_ROOT, '-o', output_dir]
 
         # When
         result = run_command(command)
@@ -36,10 +37,11 @@ class DhVirtualenvTest(TestCase):
         # Then
         self.assertEqual(0, result.returncode)
         self.assertEqual(f'{os.getcwd()}/{output_dir}/test-project_1.0.0-1_all.deb\n', result.stdout)
+        self.assertTrue(check_files_exist(result.stdout))
 
     def test_dh_virtualenv_when_absolute_output_dir_specified(self):
         # Given
-        command = [f'{SCRIPTS_DIR}/dh-virtualenv', TEST_PROJECT_ROOT, '-o', f'{TEST_FILE_SYSTEM_ROOT}/etc/dist']
+        command = [f'{RESOURCE_ROOT}/dh-virtualenv', TEST_PROJECT_ROOT, '-o', f'{TEST_FILE_SYSTEM_ROOT}/etc/dist']
 
         # When
         result = run_command(command)
@@ -47,10 +49,11 @@ class DhVirtualenvTest(TestCase):
         # Then
         self.assertEqual(0, result.returncode)
         self.assertEqual(f'{TEST_FILE_SYSTEM_ROOT}/etc/dist/test-project_1.0.0-1_all.deb\n', result.stdout)
+        self.assertTrue(check_files_exist(result.stdout))
 
     def test_dh_virtualenv_when_service_file_specified(self):
         # Given
-        command = [f'{SCRIPTS_DIR}/dh-virtualenv', TEST_PROJECT_ROOT,
+        command = [f'{RESOURCE_ROOT}/dh-virtualenv', TEST_PROJECT_ROOT,
                    '-s', f'{TEST_PROJECT_ROOT}/service/test-project.service']
 
         # When
@@ -59,12 +62,13 @@ class DhVirtualenvTest(TestCase):
         # Then
         self.assertEqual(0, result.returncode)
         self.assertEqual(f'{TEST_PROJECT_ROOT}/dist/test-project_1.0.0-1_all.deb\n', result.stdout)
+        self.assertTrue(check_files_exist(result.stdout))
         self.assertTrue(check_file_is_in_deb(f'{TEST_PROJECT_ROOT}/dist/test-project_1.0.0-1_all.deb',
                                              'lib/systemd/system/test-project.service'))
 
     def test_dh_virtualenv_when_extra_files_specified(self):
         # Given
-        command = [f'{SCRIPTS_DIR}/dh-virtualenv', TEST_PROJECT_ROOT, '-e', f'{TEST_PROJECT_ROOT}/scripts/*']
+        command = [f'{RESOURCE_ROOT}/dh-virtualenv', TEST_PROJECT_ROOT, '-e', f'{TEST_PROJECT_ROOT}/scripts/*']
 
         # When
         result = run_command(command)
@@ -72,9 +76,21 @@ class DhVirtualenvTest(TestCase):
         # Then
         self.assertEqual(0, result.returncode)
         self.assertEqual(f'{TEST_PROJECT_ROOT}/dist/test-project_1.0.0-1_all.deb\n', result.stdout)
+        self.assertTrue(check_files_exist(result.stdout))
         self.assertTrue(check_files_matches_in_deb(f'{TEST_PROJECT_ROOT}/dist/test-project_1.0.0-1_all.deb',
                                                    [('postinst', 'test-project successfully installed'),
                                                     ('postrm', 'test-project successfully removed')]))
+
+    def test_propagates_return_code_of_command(self):
+        # Given
+        command = [f'{RESOURCE_ROOT}/dh-virtualenv', TEST_PROJECT_ROOT, '-p', '/invalid/path']
+
+        # When
+        result = run_command(command)
+
+        # Then
+        self.assertEqual(127, result.returncode)
+        self.assertEqual('', result.stdout)
 
 
 if __name__ == '__main__':
--- python/tests/wheelTest.py
+++ python/tests/wheelTest.py
@@ -3,8 +3,8 @@ import shutil
 import unittest
 from unittest import TestCase
 
-from utils import TEST_PROJECT_ROOT, TEST_RESOURCE_ROOT, TEST_FILE_SYSTEM_ROOT, SCRIPTS_DIR, delete_directory, \
-    run_command
+from utils import TEST_PROJECT_ROOT, TEST_RESOURCE_ROOT, TEST_FILE_SYSTEM_ROOT, RESOURCE_ROOT, delete_directory, \
+    run_command, check_files_exist
 
 
 class WheelTest(TestCase):
@@ -16,7 +16,7 @@ class WheelTest(TestCase):
 
     def test_wheel_when_no_output_dir_specified(self):
         # Given
-        command = [f'{SCRIPTS_DIR}/wheel', TEST_PROJECT_ROOT]
+        command = [f'{RESOURCE_ROOT}/wheel', TEST_PROJECT_ROOT]
 
         # When
         result = run_command(command)
@@ -24,11 +24,12 @@ class WheelTest(TestCase):
         # Then
         self.assertEqual(0, result.returncode)
         self.assertEqual(f'{TEST_PROJECT_ROOT}/dist/test_project-1.0.0-py3-none-any.whl\n', result.stdout)
+        self.assertTrue(check_files_exist(result.stdout))
 
     def test_wheel_when_relative_output_dir_specified(self):
         # Given
         output_dir = 'tests/test_root/etc/dist' if os.path.exists('tests') else 'test_root/etc/dist'
-        command = [f'{SCRIPTS_DIR}/wheel', TEST_PROJECT_ROOT, '-o', output_dir]
+        command = [f'{RESOURCE_ROOT}/wheel', TEST_PROJECT_ROOT, '-o', output_dir]
 
         # When
         result = run_command(command)
@@ -36,10 +37,11 @@ class WheelTest(TestCase):
         # Then
         self.assertEqual(0, result.returncode)
         self.assertEqual(f'{os.getcwd()}/{output_dir}/test_project-1.0.0-py3-none-any.whl\n', result.stdout)
+        self.assertTrue(check_files_exist(result.stdout))
 
     def test_wheel_when_absolute_output_dir_specified(self):
         # Given
-        command = [f'{SCRIPTS_DIR}/wheel', TEST_PROJECT_ROOT, '-o', f'{TEST_FILE_SYSTEM_ROOT}/etc/dist']
+        command = [f'{RESOURCE_ROOT}/wheel', TEST_PROJECT_ROOT, '-o', f'{TEST_FILE_SYSTEM_ROOT}/etc/dist']
 
         # When
         result = run_command(command)
@@ -47,6 +49,18 @@ class WheelTest(TestCase):
         # Then
         self.assertEqual(0, result.returncode)
         self.assertEqual(f'{TEST_FILE_SYSTEM_ROOT}/etc/dist/test_project-1.0.0-py3-none-any.whl\n', result.stdout)
+        self.assertTrue(check_files_exist(result.stdout))
+
+    def test_propagates_return_code_of_command(self):
+        # Given
+        command = [f'{RESOURCE_ROOT}/wheel', TEST_PROJECT_ROOT, '-p', '/invalid/path']
+
+        # When
+        result = run_command(command)
+
+        # Then
+        self.assertEqual(127, result.returncode)
+        self.assertEqual('', result.stdout)
 
 
 if __name__ == '__main__':
