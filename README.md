acc2a3aa6b8b322c6f0d70ca81c2bac2996f4a80
Attila Gombos <attila.gombos@effective-range.com>
2024-08-26 15:16:42 +0200

    Release Debian package

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
+
+[![Test and Release](https://github.com/EffectiveRange/packaging-tools/actions/workflows/test_and_release.yml/badge.svg)](https://github.com/EffectiveRange/packaging-tools/actions/workflows/test_and_release.yml)
+
 # packaging-tools
+
 Tools for packaging libraries and applications
 
 ## Python
--- build.sh
+++ build.sh
@@ -1,17 +1,27 @@
-#!/bin/bash
-cd $(dirname $0)
+#!/bin/bash -ex
+
+SCRIPTS_DIR=$(dirname "$0")
+ABSOLUTE_SCRIPTS_DIR=$(cd "$SCRIPTS_DIR" && pwd)
+
+cd "$ABSOLUTE_SCRIPTS_DIR"
 rm -rf dist
 mkdir dist
 
-IGNORE_EXT="cfg|md"
-
-for stacks in $(find  . -maxdepth 1 -type d  ! -name '.*' ! -name 'dist')
+find "$ABSOLUTE_SCRIPTS_DIR" -maxdepth 2 -name "pack_*" | while read -r script_path
 do
-    for files in $(find  $stacks -maxdepth 1 -type f  | grep -vE "\.($IGNORE_EXT)$")
-    do
-        cp -v $files dist/
-    done
+  script_name=$(basename "$script_path")
+  cp -v "$script_path" "dist/$script_name"
 done
 
 cd dist
 tar -cvzf packaging-tools.tar.gz ./*
+
+cd "$ABSOLUTE_SCRIPTS_DIR"
+rm -rf debian/usr/local/bin
+mkdir -p debian/usr/local/bin
+
+cp -v dist/pack_* debian/usr/local/bin
+
+VERSION="$(grep Version: debian/DEBIAN/control | cut -d' ' -f2)"
+
+dpkg-deb -Zxz --root-owner-group --build debian "dist/packaging-tools_$VERSION-1_all.deb"
