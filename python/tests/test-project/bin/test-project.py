5cdfb2284ee5c55ab9f839f36268eb378b01d1c4
AttilaGombosER <attila.gombos@effective-range.com>
2025-09-26 17:47:03 +0200

    Run in buildroot when needed (#36)

--- python/tests/test-project/bin/test-project.py
+++ python/tests/test-project/bin/test-project.py
@@ -3,12 +3,12 @@ from test_module import TestClass
 
 
 def main() -> None:
-    setup_logging('test-project')
+    setup_logging("test-project")
 
     test_class = TestClass()
 
     test_class.test_method()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
